ad0d5869afa76bfb4c70fc9875975cf9afbf6256
Julia Miho <31131936+juls12@users.noreply.github.com>
2020-12-06 12:59:36 +0200

    react router added

--- src/App.js
+++ src/App.js
@@ -12,10 +12,20 @@ function App() {
     <Router>
     <div className="App">
      <Nav />
+     <Switch>
+     <Route path="/" exact component={Home}/>
      <Route path="/courses" component={Courses} />
+     <Route path="/addcourse" component={AddCourse} />
+     </Switch>
     </div>
     </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './index.css';
+import {Link} from 'react-router-dom';
 
-function Nav () {
+function Nav() {
     return (
         <nav>
             <h3>Logo</h3>
-            <ul>
-                <li>About</li>
-                <li>Shop</li>
+            <ul className="nav-links">
+                <Link to="/courses">
+                    <li>Courses</li>
+                </Link>
+                <Link to="/addcourse">
+                    <li>AddCourse</li>
+                </Link>
             </ul>
         </nav>
     )
