b6d0346aff3610fe3eb752253a999aaac8cac97a
Julia Miho <31131936+juls12@users.noreply.github.com>
2020-12-19 17:44:49 +0200

    get instuctors + edit modal (with edit mode)

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -2,30 +2,43 @@ import React from 'react';
 import axios from 'axios';
 import { Container } from 'reactstrap';
 import "../css/courses.css"
+import AddCourse from './AddCourse';
 import { BsCheck, BsX } from "react-icons/bs";
-import { Card, CardImg, CardText, CardBody,CardTitle, Button } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Button } from 'reactstrap';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 export default class Course extends React.Component {
     state = {
         course: {},
         instructors: [],
-        modalisOpen: false
+        modalisOpenEdit: false,
+        modalisOpenDelete: false
     };
+
+    encodeQueryData(param, data) {
+        const ret = [];
+        for (let d in data)
+            ret.push(encodeURIComponent(param) + '=' + encodeURIComponent(data[d]));
+        return ret.join('&');
+    }
+
     toggleModalEdit() {
         this.setState({
-            modalisOpen: !this.state.modalisOpen
+            modalisOpenEdit: !this.state.modalisOpenEdit
         });
     }
+
     toggleModalDelete() {
         this.setState({
-            modalisOpen: !this.state.modalisOpen
+            modalisOpenDelete: !this.state.modalisOpenDelete
         });
     }
-    closeModal() {
+
+    closeModalDelete() {
         this.setState({
-            modalisOpen: false
+            modalisOpenDelete: false
         })
     }
+
     // Server runs on port 3001
     //Get Course id and the data for their instructors
     componentDidMount() {
@@ -34,15 +47,20 @@ export default class Course extends React.Component {
             .then(res => {
                 console.log(res.data);
                 this.setState({ course: res.data });
+                return res.data
             })
-            .then(() => {
-                axios.get(`http://localhost:3001/courses/${params.id}/instructors/`)
-                    .then(res => {
-                        console.log(res.data);
-                        this.setState({ instructors: res.data });
-                    })
+            .then(course => {
+                const queryParams = this.encodeQueryData('id', course.instructors)
+                if (queryParams) {
+                    axios.get(`http://localhost:3001/instructors?${queryParams}`)
+                        .then(res => {
+                            console.log(res.data);
+                            this.setState({ instructors: res.data });
+                        })
+                }
             });
     }
+
     deleteCourse(e) {
         const { match: { params } } = this.props;
         e.preventDefault();
@@ -61,6 +79,7 @@ export default class Course extends React.Component {
                 console.log(err);
             })
     }
+
     render() {
         const convertDate = function (dateString) {
             if (dateString) {
@@ -74,38 +93,44 @@ export default class Course extends React.Component {
                 <Card>
                     <CardTitle><h1>{this.state.course.title}</h1></CardTitle>
                     <CardBody>
-                        <CardImg src={this.state.course.imagePath} alt="Card image cap"/>
+                        <CardImg src={this.state.course.imagePath} alt="Card image cap" />
                         <CardText>
                             <span style={{ fontWeight: "bold" }}>Price: {this.state.course.price?.normal}‎€</span>
-                            <br /> <br/> 
+                            <br /> <br />
                             <span style={{ fontWeight: "bold" }}> Bookable: {this.state.course.open ? <BsCheck /> : <BsX />}</span>
-                            <br/> <br/> 
+                            <br /> <br />
                             <span style={{ fontWeight: "bold" }}> Duration: {this.state.course.duration}</span>
-                            <br/> <br/> 
+                            <br /> <br />
                             <span style={{ fontWeight: "bold" }}> Dates: {convertDate(this.state.course.dates?.start_date)} - {convertDate(this.state.course.dates?.end_date)} </span>
-                            <br/> <br/> 
+                            <br /> <br />
                             <span style={{ fontWeight: "bold" }} dangerouslySetInnerHTML={{ __html: this.state.course.description }}></span>
                             <Button color="primary" onClick={this.toggleModalEdit.bind(this)}>Edit</Button>
-                            <button className="btn btn-danger m-1" onClick={this.toggleModalDelete.bind(this)}> Delete</button>
+
+                            <Modal isOpen={this.state.modalisOpenEdit} >
+                                <ModalHeader toggle={this.toggleModalEdit.bind(this)}>Edit Course</ModalHeader>
+                                <ModalBody><AddCourse isEditMode={true} newCourse={this.state.course} /></ModalBody>
+                            </Modal>
+
+                            <Button color="danger" className="ml-3" onClick={this.toggleModalDelete.bind(this)}>Delete</Button>
                             <br />
-                            <Modal isOpen={this.state.modalisOpen}>
+                            <Modal isOpen={this.state.modalisOpenDelete}>
                                 <ModalHeader toggle={this.toggleModalDelete.bind(this)}>Delete Course</ModalHeader>
                                 <ModalBody>
                                     <h2>Are sure you want to delete this {this.state.course.title} course?</h2>
                                 </ModalBody>
                                 <ModalFooter>
                                     <Button color="primary" onClick={this.deleteCourse.bind(this)}>Delete</Button>{' '}
-                                    <Button color="secondary" onClick={this.closeModal.bind(this)} >Cancel</Button>
+                                    <Button color="secondary" onClick={this.closeModalDelete.bind(this)} >Cancel</Button>
                                 </ModalFooter>
                             </Modal>
                         </CardText>
                         <CardTitle tag="h3">Instructors</CardTitle>
-                        {this.state.instructors.map(({ id, gender, name, email, dob, bio, linkedin, hobbies }) => (
+                        {this.state.instructors.map(({ id, name, email, dob, bio, linkedin }) => (
                             <CardText key={id}>
                                 <span style={{ fontWeight: "bold" }}> {name.first} {name.last} ({dob}) </span>
-                               <br/>
+                                <br />
                                 <span style={{ fontWeight: "bold" }}> <a href={email}>{email}</a> | <a href={linkedin}>Linkedin</a></span>
-                                <br/>
+                                <br />
                                 <span style={{ fontWeight: "bold" }}> {bio} </span>
                             </CardText>
                         ))}
