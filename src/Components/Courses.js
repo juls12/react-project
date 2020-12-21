0d615d9677bdd45e1631c9e73a15430d412b4af6
Julia Miho <31131936+juls12@users.noreply.github.com>
2020-12-21 22:22:10 +0200

    add search courses

--- src/Components/Courses.js
+++ src/Components/Courses.js
@@ -5,14 +5,10 @@ import { Button, Container, UncontrolledTooltip } from 'reactstrap';
 import { BsCheck, BsX } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 
-
-// import {
-//   Card, CardText, CardBody, CardLink,
-//   CardTitle, CardSubtitle
-// } from 'reactstrap';
-
 const Courses = () => {
     const [courses, setCourses] = useState([]);
+
+    const [searchText, setSearchField] = useState('');
 
     useEffect(() => {
         fetch("http://localhost:3001/courses")
@@ -25,7 +21,6 @@ const Courses = () => {
     const convertDate = function (dateString) {
         return dateString.split("-").reverse().join("-");
     }
-
     const generateTooltipId = function (id) {
         return "tooltipId_" + id;
     }
@@ -33,43 +28,49 @@ const Courses = () => {
         return `course/${id}`
     }
 
-    
+    const filterCourses = ({ target }) => {
+        const searchText = target.value;
+        console.log(searchText);
+
+        setSearchField(searchText);
+    }
 
     return (
-
         <div>
             <h1 className="text-center text-focus-in">All Courses </h1>
 
             <div className="cards">
                 <Container fluid>
                     <Col xs="12">
+                        <section> <input className="searchbox" placeholder="Search Course" onChange={filterCourses} /></section>
                         <Row>
-                            {courses.map(({ id, title, imagePath, price, duration, open, dates }) => (
-                                <Card style={{ width: '20rem' }} key={id} className="box">
-                                    <Card.Title>{title}</Card.Title>
-                                    <span id={generateTooltipId(id)}></span>
-                                    <UncontrolledTooltip target={generateTooltipId(id)}></UncontrolledTooltip>
-                                    <Card.Img variant="top" src={imagePath} />
-                                    <Card.Body>
-                                        <Card.Text>
-                                            <span style={{ fontWeight: "bold" }}> Price:  {price.normal} | Bookable: {open ? <BsCheck    /> :  <BsX    />   }</span>
-                                           <br/>
-                                            <span style={{ fontWeight: "bold" }}> Duration: {duration}  </span>
-                                            <br/>
-                                            <span style={{ fontWeight: "bold" }}> Dates: {convertDate(dates.start_date)} - {convertDate(dates.end_date)} </span>
-                                        </Card.Text>
-                                        <Button style={{ float: 'right' }} color="primary" tag={Link} to={generateCourseLink(id)} >View</Button>
-                                    </Card.Body>
-                                </Card>
-                            ))}
+                            {
+                                courses.filter(course => {
+                                    return course.title.toLowerCase().includes(searchText.toLowerCase())
+                                }).map(({ id, title, imagePath, price, duration, open, dates }) => (
+                                    <Card style={{ width: '20rem' }} key={id} className="box">
+                                        <Card.Title>{title}</Card.Title>
+                                        <span id={generateTooltipId(id)}></span>
+                                        <UncontrolledTooltip target={generateTooltipId(id)}></UncontrolledTooltip>
+                                        <Card.Img variant="top" src={imagePath} />
+                                        <Card.Body>
+                                            <Card.Text>
+                                                <span style={{ fontWeight: "bold" }}> Price:  {price.normal} | Bookable: {open ? <BsCheck /> : <BsX />}</span>
+                                                <br />
+                                                <span style={{ fontWeight: "bold" }}> Duration: {duration}  </span>
+                                                <br />
+                                                <span style={{ fontWeight: "bold" }}> Dates: {convertDate(dates.start_date)} - {convertDate(dates.end_date)} </span>
+                                            </Card.Text>
+                                            <Button style={{ float: 'right' }} color="primary" tag={Link} to={generateCourseLink(id)} >View</Button>
+                                        </Card.Body>
+                                    </Card>
+                                ))}
                         </Row>
                     </Col>
                 </Container>
             </div>
-
         </div>
     );
-
 };
 
 export default Courses;
